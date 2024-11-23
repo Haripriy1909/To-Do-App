b5993d344a3515a46d170011bb30a782eb92238c
Haripriy1909 <haripriydarji1909@gmail.com>
2024-11-23 17:19:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const taskInput = document.getElementById("taskInput");
+const taskList = document.getElementById("taskList");
+function addTask() {
+    if (taskInput.value === '') {
+        alert("please write any task");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = taskInput.value;
+        taskList.appendChild(li);
+        let span = document.createElement("sapn");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    taskInput.value = '';
+
+}
+
+taskList.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
+        e.target.clasList.toggle("checked");
+    }
+    else if (e.target.tagName = "SPAN") {
+        e.target.parentElement.remove();
+
+    }
+
+}, false);
